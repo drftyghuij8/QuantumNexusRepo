7d1b802dd2cbfbe4f3f2b02d9423434a3be81d48
drftyghuij8 <gs1link4233@hotmail.com>
2024-05-11 18:32:19 +0700

    hello world commit

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-function countConsonants(str) {
-  return str.match(/[bcdfghjklmnpqrstvwxyz]/gi).length;
+function combine(n, k) {
+  const result = [];
+  backtrack(1, []);
+  return result;
+  function backtrack(start, current) {
+    if (current.length === k) {
+      result.push([...current]);
+      return;
+    }
+    for (let i = start; i <= n; i++) {
+      current.push(i);
+      backtrack(i + 1, current);
+      current.pop();
+    }
+  }
 }
